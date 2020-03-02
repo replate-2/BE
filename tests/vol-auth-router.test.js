dc1532358cc27941b2ae2820db9c51b13fcd7ba3
mlazzell17@gmail.com <MicheSi>
2020-03-02 14:17:42 -0700

    added tests for volunteer auth router

---
+++ tests/vol-auth-router.test.js
@@ -0,0 +1,80 @@
+const request = require('supertest');
+const server = require('../api/server');
+const db = require('../data/dbConfig');
+
+describe('volunteer auth router', function () {
+    it('tests are running', function () {
+        expect(true).toBe(true);
+    })
+
+    describe('volunteer register endpoint', function () {
+        // afterEach(() => {
+        //     return db('volunteers').cleanup();
+        // })
+
+        // it('returns 201 on successful register', function () {
+        //     return request(server)
+        //         .post('/api/auth/volunteer/register')
+        //         .send({
+        //             username: 'TestVolunteer',
+        //             password: 'password',
+        //             name: 'Test Volunteer',
+        //             phoneNumber: 1234567890
+        //         })
+        //         .then(res => {
+        //             expect(res.status).toBe(201);
+        //         })
+        // })
+
+        it('returns 500 if missing required field', function () {
+            return request(server)
+                .post('/api/auth/volunteer/register')
+                .send({
+                    username: 'TestingV',
+                    password: 'password',
+                    name: 'Test Volunteer'
+                })
+                .then(res => {
+                    expect(res.status).toBe(500);
+                })
+        })
+    })
+
+    describe('business login endpoint', function () {
+        it('returns 200 on successful login', function () {
+            return request(server)
+                .post('/api/auth/volunteer/login')
+                .send({
+                    username: 'TestVol',
+                    password: 'password'
+                })
+                .then(res => {
+                    expect(res.status).toBe(200);
+                })
+        })
+
+        it('returns token and welcome message on successful login', function () {
+            return request(server)
+                .post('/api/auth/business/login')
+                .send({
+                    username: 'TestVol',
+                    password: 'password'
+                })
+                .then(res => {
+                    expect(res.body).toEqual({message: res.body.message, token: res.body.token})
+                })
+        })
+
+        it('returns 401 if incorrect credentials', function () {
+            return request(server)
+            .post('/api/auth/business/login')
+            .send({
+                username: 'TestVol',
+                password: 'wrongpassword'
+            })
+            .then(res => {
+                expect(res.status).toBe(401);
+            })
+        })
+    })
+})
