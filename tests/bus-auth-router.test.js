23d452d3cfe0a4e387de861d4bf2d6c7623942a8
mlazzell17@gmail.com <MicheSi>
2020-03-02 11:33:55 -0700

    fixed add on businesses and volunteers

--- tests/bus-auth-router.test.js
+++ tests/bus-auth-router.test.js
@@ -1,8 +1,30 @@
 const request = require('supertest');
 const server = require('../api/server');
+const db = require('../data/dbConfig');
 
 describe('business auth router', function () {
     it('tests are running', function () {
         expect(true).toBe(true);
+    })
+
+    describe('business register endpoint', function () {
+        beforeEach(() => {
+            return db('businesses').truncate();
+        })
+
+        it('returns 201 on successful register', function () {
+            return request(server)
+                .post('/api/auth/business/register')
+                .send({
+                    username: 'TestBusiness',
+                    password: 'password',
+                    businessName: 'TestBusiness',
+                    businessAddress: '123 N. Street',
+                    businessPhone: 1234567890
+                })
+                .then(res => {
+                    expect(res.status).toBe(201);
+                })
+        })
     })
 })
