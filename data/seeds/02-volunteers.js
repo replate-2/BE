d94907491782f77bcc612852c8120fabe9c77dbe
mlazzell17@gmail.com <MicheSi>
2020-03-03 10:56:22 -0700

    change to seeds

--- data/seeds/02-volunteers.js
+++ data/seeds/02-volunteers.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('volunteers').insert([
-        {id: 1, username: 'Volunteer1', password: 'password', name: 'Volunteer1', phoneNumber: '5551234567'},
-        {id: 2, username: 'Volunteer2', password: 'password', name: 'Volunteer2', phoneNumber: '5551234567'},
-        {id: 3, username: 'Volunteer3', password: 'password', name: 'Volunteer3', phoneNumber: '5551234567'}
+        {id: 1, username: 'Volunteer1', password: 'password', name: 'Volunteer1', phoneNumber: '1234567890'},
+        {id: 2, username: 'Volunteer2', password: 'password', name: 'Volunteer2', phoneNumber: '7894561230'},
+        {id: 3, username: 'Volunteer3', password: 'password', name: 'Volunteer3', phoneNumber: '7891234560'}
       ]);
     });
 };
