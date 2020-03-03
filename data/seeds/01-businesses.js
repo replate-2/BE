ed9921c271bec5cc66c5a8a8bd9244bb6f9a3d0c
mlazzell17@gmail.com <MicheSi>
2020-03-03 11:10:03 -0700

    fixed seed

--- data/seeds/01-businesses.js
+++ data/seeds/01-businesses.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('businesses').insert([
-        {id: 1, username: 'Business1', password: 'password', businessName: 'Business1', businessAddress: '123 N. Street', businessPhone: '1234567890'},
-        {id: 2, username: 'Business2', password: 'password', businessName: 'Business2', businessAddress: '456 S. Street', businessPhone: '1234567890'},
-        {id: 3, username: 'Business3', password: 'password', businessName: 'Business3', businessAddress: '1234 W. Street', businessPhone: '1234567890'}
+        {id: 1, username: 'Business1', password: 'password', businessName: 'Business1', businessAddress: '123 N. Street', businessPhone: 1234567890},
+        {id: 2, username: 'Business2', password: 'password', businessName: 'Business2', businessAddress: '456 S. Street', businessPhone: 1234567890},
+        {id: 3, username: 'Business3', password: 'password', businessName: 'Business3', businessAddress: '1234 W. Street', businessPhone: 1234567890}
       ]);
     });
 };
